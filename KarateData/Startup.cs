4e546b057446744eb52a8e8e3f2747248a225fc9
Aleksandar Stojanoski <aleksandar-stojanoski@hotmail.com>
2017-12-25 02:17:12 +0100

    Add project files.

---
+++ KarateData/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KarateData.Startup))]
+namespace KarateData
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
